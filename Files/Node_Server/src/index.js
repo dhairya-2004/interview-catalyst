176c218dbfb1ed14b1e85dcb376925017c59bd7d
jash2504 <asmanijash51@gmail.com>
2024-02-15 22:22:04 +0530

    1

--- Files/Node_Server/src/index.js
+++ Files/Node_Server/src/index.js
@@ -13,11 +13,12 @@ const configcors = {
   credential: true,
   method: ["GET", "POST", "PUT", "DELETE"],
 };
-app.options("", cors(configcors));
-app.use(cors(configcors));
 
 const app = express();
 const port = 5000;
+
+app.options("", cors(configcors));
+app.use(cors(configcors));
 
 app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ extended: true, limit: "50mb" }));
